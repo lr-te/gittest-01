c4f5299eb480f624d9b820bca571af61fa571411
tantan <sjt860284616@163.com>
2020-11-01 09:27:54 +0800

    git提交测试01 20-11-01

---
+++ test01-git/src/demo/DeomClass.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class DeomClass {
+    public static void main(String[] args) {
+        System.out.println("Git测试1");
+        System.out.println("Git测试2");
+        System.out.println("Git测试3");
+        System.out.println("Git测试4");
+        System.out.println("Git测试5");
+        System.out.println("Git测试6");
+
+    }
+    public  void  test(){
+        int i=0;
+        i +=10;
+
+    }
+}
